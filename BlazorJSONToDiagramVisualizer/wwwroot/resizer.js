5f4a159c5930160342e0457138e62e1795df9e53
SatishRaj4377 <158574671+SatishRaj4377@users.noreply.github.com>
2025-04-08 19:02:42 +0530

    first commit

---
+++ BlazorJSONToDiagramVisualizer/wwwroot/resizer.js
@@ -0,0 +1,18 @@
+﻿window.attachResizeEvents = (dotNetHelper) => {
+    function mouseMoveHandler(e) {
+        dotNetHelper.invokeMethodAsync('OnMouseMove', e.clientX);
+    }
+
+    function mouseUpHandler(e) {
+        dotNetHelper.invokeMethodAsync('OnMouseUp');
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+    }
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+}
+
+window.getWindowWidth = () => {
+    return window.innerWidth;
+};
+
