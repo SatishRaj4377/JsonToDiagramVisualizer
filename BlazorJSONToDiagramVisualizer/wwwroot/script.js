baacc47efc881aa75e2aea03e1a510a30ecb3e66
SatishRaj4377 <158574671+SatishRaj4377@users.noreply.github.com>
2025-04-13 18:50:30 +0530

    Implemented Navbar and Import -  Export Functionality

--- BlazorJSONToDiagramVisualizer/wwwroot/script.js
+++ BlazorJSONToDiagramVisualizer/wwwroot/script.js
@@ -45,3 +45,51 @@ window.getTextSize = (text, font) => {
         height: height
     };
 };
+
+window.exportJsonFile = (data, filename) => {
+    try {
+        const blob = new Blob([data], { type: "application/json" });
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = filename || "data.json";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    } catch (err) {
+        console.error("Export failed:", err);
+    }
+};
+
+
+window.importJsonFromFile = (dotNetHelper) => {
+    if (!dotNetHelper || !dotNetHelper.invokeMethodAsync) {
+        console.warn("DotNet helper is not ready.");
+        return;
+    }
+
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = ".json";
+    input.style.display = "none";
+
+    input.onchange = async (e) => {
+        const file = e.target.files[0];
+        if (!file) return;
+
+        const reader = new FileReader();
+        reader.onload = function () {
+            const content = reader.result;
+
+            try {
+                dotNetHelper.invokeMethodAsync("ReceiveImportedJson", content);
+            } catch (err) {
+                console.error("Interop error: ", err);
+            }
+        };
+        reader.readAsText(file);
+    };
+
+    document.body.appendChild(input);
+    input.click();
+    document.body.removeChild(input);
+};
