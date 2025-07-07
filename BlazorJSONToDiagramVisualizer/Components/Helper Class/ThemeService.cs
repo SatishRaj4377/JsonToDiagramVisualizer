c2b11289e877e92bbd95b3028ffea59abe8edc1f
SatishRaj4377 <158574671+SatishRaj4377@users.noreply.github.com>
2025-07-07 11:16:44 +0530

    fixed color in search action highlighting

--- BlazorJSONToDiagramVisualizer/Components/Helper Class/ThemeService.cs
+++ BlazorJSONToDiagramVisualizer/Components/Helper Class/ThemeService.cs
@@ -20,7 +20,6 @@
         public string CssUrl { get; set; }
         public string DiagramBackgroundColor { get; set; }
         public string GridlinesColor { get; set; }
-        public string HighlightColor { get; set; }
         public string NodeFillColor { get; set; }
         public string NodeStrokeColor { get; set; }
         public string TextKeyColor { get; set; }
@@ -36,6 +35,9 @@
         public string PopupKeyColor { get; set; }
         public string PopupValueColor { get; set; }
         public string PopupContentBGColor { get; set; }
+        public string HighlightFillColor { get; set; }
+        public string HighlightFocusColor { get; set; }
+        public string HighlightStrokeColor { get; set; }
 
         public ThemeSettings(string theme = "light")
         {
@@ -44,14 +46,13 @@
                 CssUrl = "https://cdn.syncfusion.com/blazor/29.1.33/styles/tailwind.css";
                 DiagramBackgroundColor = "#F8F9FA";
                 GridlinesColor = "#EBE8E8";
-                HighlightColor = "#e7f0e6";
                 NodeFillColor = "rgb(255, 255, 255)";
                 NodeStrokeColor = "rgb(188, 190, 192)";
                 TextKeyColor = "#A020F0";
                 TextValueColor = "rgb(83, 83, 83)";
                 TextValueNullColor = "rgb(41, 41, 41)";
                 ExpandIconFillColor = "#e0dede";
-                ExpandIconColor = " rgb(46, 51, 56)";
+                ExpandIconColor = "rgb(46, 51, 56)";
                 ExpandIconBorder = "rgb(188, 190, 192)";
                 ConnectorStrokeColor = "rgb(188, 190, 192)";
                 ChildCountColor = "rgb(41, 41, 41)";
@@ -59,29 +60,34 @@
                 NumericColor = "rgb(182, 60, 30)";
                 PopupKeyColor = "#5C940D";
                 PopupValueColor = "#1864AB";
-                PopupContentBGColor = "#f0f0f0";
+                PopupContentBGColor = "#F8F9FA";
+                HighlightFillColor = "rgba(27, 255, 0, 0.1)";
+                HighlightFocusColor = "rgba(252, 255, 166, 0.57)";
+                HighlightStrokeColor = "rgb(0, 135, 54)";
             }
             else
             {
                 CssUrl = "https://cdn.syncfusion.com/blazor/29.1.33/styles/tailwind-dark.css";
                 DiagramBackgroundColor = "#1e1e1e";
-                GridlinesColor = "rgb(40, 40, 40)";
-                HighlightColor = "rgb(32, 97, 51, 0.5)";
+                GridlinesColor = "rgb(45, 45, 45)";
                 NodeFillColor = "rgb(41, 41, 41)";
                 NodeStrokeColor = "rgb(66, 66, 66)";
                 TextKeyColor = "#4dabf7";
                 TextValueColor = "rgb(207, 227, 225)";
                 TextValueNullColor = "rgb(151, 150, 149)";
                 ExpandIconFillColor = "#1e1e1e";
-                ExpandIconBorder = "rgb(66, 66, 66)";
                 ExpandIconColor = "rgb(220, 221, 222)";
+                ExpandIconBorder = "rgb(66, 66, 66)";
                 ConnectorStrokeColor = "rgb(66, 66, 66)";
                 ChildCountColor = "rgb(255, 255, 255)";
                 BooleanColor = "rgb(61, 226, 49)";
                 NumericColor = "rgb(232, 196, 121)";
                 PopupKeyColor = "#A5D8FF";
                 PopupValueColor = "#40C057";
-                PopupContentBGColor = "#01000c57;";
+                PopupContentBGColor = "#1A1A1A";
+                HighlightFillColor = "rgba(27, 255, 0, 0.1)";
+                HighlightFocusColor = "rgba(82, 102, 0, 0.61)";
+                HighlightStrokeColor = "rgb(0, 135, 54)";
             }
         }
     }
